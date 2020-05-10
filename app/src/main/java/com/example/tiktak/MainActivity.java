515b415c558e926c9e5efaad20e2ffaf45f645af
lalit_kumar <lalitrajput069@gmail.com>
2020-05-10 11:37:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tiktak/MainActivity.java
@@ -0,0 +1,148 @@
+package com.example.tiktak;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView tabimage;
+    GridLayout gridLayout;
+    boolean One, Two,gameWinner;
+
+    enum Player {
+        One, Two
+    }
+
+    private TextView scoreOne, scoreTwo;
+    private int playerOne, playerTwo;
+    String winnerOfGame;
+
+    private Player currentPlayer = Player.One;
+    private Player[] nowPlaying = new Player[9];
+    private int[][] winner = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 4, 8},
+            {2, 4, 6}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}}; // this all are the winning conditions
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridLayout = findViewById(R.id.gridlayout);
+        scoreOne = findViewById(R.id.scoreone);
+        scoreTwo = findViewById(R.id.scoretwo);
+        scoreOne.setText(playerOne + "");
+        scoreTwo.setText(playerTwo + "");
+    }
+
+    public void clickit(View imageview) {
+        tabimage = (ImageView) imageview;
+        int tagValue = Integer.parseInt(tabimage.getTag().toString());
+
+        if (nowPlaying[tagValue] == null) {   // this condition stop image view to assign another image if there is already image inside it.
+            nowPlaying[tagValue] = currentPlayer; // this tell which player is playing now
+            if (currentPlayer == Player.One) {
+                tabimage.setImageResource(R.drawable.x); // here we assigning the X image in every ImageView when it clicks by the player.
+                currentPlayer = Player.Two;
+            } else if (currentPlayer == Player.Two) {
+                tabimage.setImageResource(R.drawable.zero); // here we assigning the O image in every image ImageView it clicks by the player.
+                currentPlayer = Player.One;
+            }
+        }
+        for (int[] test : winner){  // here we extract single dimensional array from two dimensional array.
+            if (nowPlaying[test[0]] == nowPlaying[test[1]] &&
+                    nowPlaying[test[1]] == nowPlaying[test[2]] &&
+                    nowPlaying[test[2]] == Player.One &&
+                    nowPlaying[test[2]] != null) { // this condition only be true if player One wins
+                winnerOfGame = "Player One win";
+                One = true;
+                gameWinner=true;
+                playerOne++;  // this is a score teller. Mean how many time player one wins.
+                scoreOne.setText(playerOne + "");
+
+
+            } else if (nowPlaying[test[0]] == nowPlaying[test[1]] &&
+                    nowPlaying[test[1]] == nowPlaying[test[2]] &&
+                    nowPlaying[test[2]] == Player.Two &&
+                    nowPlaying[test[1]] != null) { //this condition only be true if player Two wins
+                winnerOfGame = "Player Two win";
+                Two = true;
+                gameWinner=true;
+                playerTwo++; // this is a score teller. Mean how many times player two wins.
+                scoreTwo.setText(playerTwo + "");
+            }
+            if (One == true) {
+                restart();  //this function restart or exit the game according to user choice after any of one player wins.
+                One = false;
+            } else if (Two == true) {
+                restart();
+                Two = false;
+            }
+        }
+        if (nowPlaying[0] != null && nowPlaying[1] != null && nowPlaying[2] != null &&
+                nowPlaying[3] != null && nowPlaying[4] != null && nowPlaying[5] != null && nowPlaying[6] != null &&
+                nowPlaying[7] != null && nowPlaying[8] != null&&gameWinner==false&&gameWinner==false) {
+            AlertDialog.Builder draw=new AlertDialog.Builder(MainActivity.this);
+            draw.setMessage("Match is Draw").setPositiveButton("Play again", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    for (int i = 0; i < gridLayout.getChildCount(); i++) {
+                        ImageView image = (ImageView) gridLayout.getChildAt(i);
+                        image.setImageDrawable(null);
+                    }
+                    nowPlaying = new Player[9];
+                    currentPlayer=Player.One;
+                }
+            }).setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                    System.exit(0);
+                }
+            }).setCancelable(false);
+            AlertDialog finish=draw.create();
+            finish.show();
+        }
+    }
+
+
+    public void restart() {
+        AlertDialog.Builder alertBox=new AlertDialog.Builder(MainActivity.this);
+        alertBox.setMessage(winnerOfGame).setPositiveButton("Play again", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                for (int i = 0; i < gridLayout.getChildCount(); i++) {
+                    ImageView image = (ImageView) gridLayout.getChildAt(i);
+                    image.setImageDrawable(null);
+            }
+                nowPlaying = new Player[9];
+        }
+        }).setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                    System.exit(0);
+            }
+        }).setCancelable(false);
+        AlertDialog finish=alertBox.create();
+        finish.show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        final AlertDialog.Builder exit=new AlertDialog.Builder(MainActivity.this);
+        exit.setMessage("Are You Sure?").setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.super.onBackPressed();
+        }}).setNegativeButton("Cancel",null).setCancelable(false);
+        AlertDialog finish=exit.create();
+        finish.show();
+    }
+}
